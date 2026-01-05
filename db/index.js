fcc7f9b19f25ece6e34d7c5954dcc71c628d870e
AarohiSaxena10 <aarohisaxena1014@gmail.com>
2026-01-05 21:17:09 +0530

    supabase url added

--- db/index.js
+++ db/index.js
@@ -5,6 +5,14 @@ dotenv.config();
 const { Pool } = pg;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, 
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },   
+});
+pool.query('SELECT NOW()', (err, res) => {
+  if (err) {
+    console.error('❌ DB connection failed:', err);
+  } else {
+    console.log('✅ DB connected! Current time:', res.rows[0].now);
+  }
 });
 export default pool;
