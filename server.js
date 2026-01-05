3295adabe2fcb9b7a06d904fcc4ff41840ec7ee0
SaxenaAarohi <aarohisaxena1014@gmail.com>
2026-01-05 22:11:41 +0530

    Simplify CORS configuration to a single origin

--- server.js
+++ server.js
@@ -9,11 +9,8 @@ import getproducts from './routes/products.js';
 const app = express();
 
 app.use(cors({
-  origin: 
-    [
-    "http://localhost:3000",
-  "https://thrift-frontend.vercel.app"
-      ]
+  origin: "https://thrift-frontend.vercel.app"
+      
 }));
 
 app.use(express.json());
